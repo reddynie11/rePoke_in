6e5b337d89f815f89a2be158d88224c3f4850f5f
Narasimha Reddy <reddy.nie11@gmail.com>
2019-08-29 17:45:35 +0530

    'component_contact'

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = ()=>{
+    return(
+        <div className="container" >
+            <h2>ContactPage</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, reiciendis! Esse et corrupti sequi voluptates laborum corporis. Itaque at, distinctio aperiam voluptatem voluptatibus, minus excepturi esse tenetur obcaecati rem natus! </p>
+        </div> 
+    )
+}
+export default Contact;
