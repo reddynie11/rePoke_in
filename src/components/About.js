7b71df6c88673004299c4c581beea40d19b20dac
Narasimha Reddy <reddy.nie11@gmail.com>
2019-08-29 17:44:44 +0530

    'component_about'

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const About = ()=>{
+    return(
+        <div className="container" >
+            <h2>AboutPage</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, reiciendis! Esse et corrupti sequi voluptates laborum corporis. Itaque at, distinctio aperiam voluptatem voluptatibus, minus excepturi esse tenetur obcaecati rem natus! </p>
+        </div> 
+    )
+}
+export default About;
