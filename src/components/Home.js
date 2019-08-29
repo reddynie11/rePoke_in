d5d15e7be691fcc2fd7171a7643a37e9bfd910bb
Narasimha Reddy <reddy.nie11@gmail.com>
2019-08-29 17:43:37 +0530

    'Component_home'

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = ()=>{
+    return(
+        <div className="container" >
+            <h2>HomePage</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, reiciendis! Esse et corrupti sequi voluptates laborum corporis. Itaque at, distinctio aperiam voluptatem voluptatibus, minus excepturi esse tenetur obcaecati rem natus! </p>
+        </div> 
+    )
+}
+export default Home;
