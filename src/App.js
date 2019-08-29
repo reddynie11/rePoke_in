1090c5a331caed6fe795391799dffb8d9c667338
Narasimha Reddy <reddy.nie11@gmail.com>
2019-08-29 17:59:18 +0530

    'BrowserRouter_Route'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </BrowserRouter>
   );
 }
 
