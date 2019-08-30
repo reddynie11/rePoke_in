6a869c331f602b3da67c5dea6f7334f9d481e79c
reddynie11 <reddy.nie11@gmail.com>
2019-08-31 01:31:47 +0530

    connect redux store to component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,11 @@
 import React from 'react'
-import axios from 'axios'
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Posts extends React.Component{
-    state={
-        posts: []
-    }
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-             .then((responce)=>{
-                 this.setState({
-                    posts:responce.data.slice(0,10)
-                 })
-             })
-    }
+ 
     render(){
-        const {posts} =  this.state;
+        const {posts} =  this.props;
         const postList = posts.length ? (
             posts.map((post)=>{
                 return (
@@ -40,4 +30,10 @@ class Posts extends React.Component{
         )
     }
 }
-export default Posts;
+
+const mapStateToProps=(state)=>{
+    return {
+        posts : state.posts
+    }
+}
+export default connect(mapStateToProps)(Posts);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 const initState ={
-    posts : []
+    posts : [
+        {id:'1', title:'Best Beaches in Thailand', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur eveniet aliquam fugiat distinctio quaerat numquam vitae totam at, quibusdam facere, fuga repudiandae libero reiciendis nobis quo laudantium ab illum molestias.'},
+        {id:'2', title:'Best Food to try in Thailand', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur eveniet aliquam fugiat distinctio quaerat numquam vitae totam at, quibusdam facere, fuga repudiandae libero reiciendis nobis quo laudantium ab illum molestias.'},
+        {id:'3', title:'Best Party places in Thailand', body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur eveniet aliquam fugiat distinctio quaerat numquam vitae totam at, quibusdam facere, fuga repudiandae libero reiciendis nobis quo laudantium ab illum molestias.'}
+    ]
 }
 
 const rootReducer = (state=initState, action)=>{
